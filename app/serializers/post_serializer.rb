9b4f6e04f3a86dc22e705bf33f6b5102561efad8
Francisco <fnaveira11@gmail.com>
2021-06-25 11:27:56 -0400

    commit tree dc434cdc208907d3f574b094eb5d57d270526d31

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,12 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :title, :short_content, :tags, :content
+
+  def short_content
+    # byebug
+    "#{self.object.content[0..39]}..."
+  end
+
+  belongs_to :author
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+end
